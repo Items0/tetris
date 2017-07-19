edff5da2ab1f2d7a42b34e2366ec956facb1a5e8
Items0 <nieznany012@wp.pl>
2017-07-19 16:47:40 +0200

    Obsługa startu -> klawisz/lpm

--- javascript.js
+++ javascript.js
@@ -1,8 +1,24 @@
-function start()
+function sleep (milliseconds) 
 {
-	
+  var start = new Date().getTime();
+  while((new Date().getTime() - start) < milliseconds);
 }
 
+function start(e)
+{
+	if(e.keyCode == 32 || e.button == 0)
+	{
+		var myButton = document.getElementById('startButton');
+		for(var i = 3; i >= 1; i--)
+		{
+			myButton.textContent = i;
+			sleep(1000);
+		}
+		var which = Math.random(3).floor();
+	}
+}
+document.addEventListener('keydown', start);
+document.getElementById('startButton').addEventListener('click', start);
 function moveShape()
 {
 	
